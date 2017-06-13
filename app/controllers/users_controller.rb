48172201faab696517de72db42ce1f07e157e6c6
NickNish09 <nicholasmarques@cjr.org.br>
2017-06-13 13:29:32 -0300

    finalizei açoes do CRUD para usuario poder editar e deletar perfil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   before_action :authorize, except: [:new, :create]
+  before_action :correct_user?, only: [:edit, :update, :destroy]
   def index
       @users = User.all
   end
@@ -20,6 +21,26 @@ class UsersController < ApplicationController
   	else
   		render action: :new
   	end
+  end
+
+  def edit
+      @user = User.find(params[:id]) 
+  end
+   
+  def update
+      @user = User.find(params[:id]) 
+      if @user.update_attributes(user_params)
+        redirect_to users_path
+      else
+        render action: :edit
+      end
+  end
+   
+  def destroy
+      @user = User.find(params[:id]) 
+      @user.destroy
+      sign_out
+      redirect_to root_path
   end
 
   private
