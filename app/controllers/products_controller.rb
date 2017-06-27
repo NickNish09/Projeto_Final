43edc96eb64e734a3d6da3006507526d010f9ef8
NickNish09 <nicholasmarques@cjr.org.br>
2017-06-27 10:04:33 -0300

    terminado carrinho

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,16 +4,16 @@ class ProductsController < ApplicationController
 	end
 	def index
 		@products = Product.all
-		#@carts = Cart.find(5)
+		
 	end
 
 	def condimentos
 		@products = Product.where(:categories => 'condimentos')
-		#@carts = Cart.find(5)
+		
 	end
 	def cafedamanha
 		@products = Product.where(:categories => 'cafedamanha')
-		#@carts = Cart.find(5)
+		
 	end
 	
 	def get
